53512fb2a0cc7502ea04e3d6bde4062a5cf2f30d
zhuyali2018 <yali900d@gmail.com>
2018-06-13 06:10:46 +0100

    Change2

--- QuickReviewerMain.cpp
+++ QuickReviewerMain.cpp
@@ -22,15 +22,13 @@ int main(int argc, const char ** argv)
 		exit(0);
 	}
 	printf("XML file '%s' loaded and parsed successfully\n\n", mydoc);
-
-	int qn = 1;   //question number;
+	
 	do {
 		char * str = nullptr;
 		char * cat = nullptr;
 		
 		char name[10];
-		sprintf_s(name, "Q%d", qn++);
-		
+	
 		XMLElement* attributeQLib = doc->FirstChildElement(name);
 		if (!attributeQLib) {
 			break;
@@ -38,49 +36,6 @@ int main(int argc, const char ** argv)
 		attributeQLib->QueryStringAttribute("type", (const char **)&str);
 		attributeQLib->QueryStringAttribute("category", (const char **)&cat);
 		
-		XMLElement *Que = attributeQLib->FirstChildElement("Question");
-		const char * question = nullptr;
-		if (Que) {
-			question = Que->GetText();
-		}
-		const char * answer = nullptr;
-		Que = attributeQLib->FirstChildElement("Answer");
-		if (Que) {
-			answer = Que->GetText();
-		}
-		
-		cout << "Question No " << qn - 1 << " :" << endl;
-		printf("Question type: %s\n", str);
-		printf("Question Category: %s\n", cat);
-		printf("Question: %s\n", question);
-		
-		Que = attributeQLib->FirstChildElement("ChoiceA");
-		if (Que) {
-			const char * choiceA = Que->GetText();
-			printf("Choice A: %s\n", choiceA);
-		}
-		Que = attributeQLib->FirstChildElement("ChoiceB");
-		if (Que) {
-			const char * choiceB = Que->GetText();
-			printf("Choice B: %s\n", choiceB);
-		}
-		Que = attributeQLib->FirstChildElement("ChoiceC");
-		if (Que) {
-			const char * choiceC = Que->GetText();
-			printf("Choice C: %s\n", choiceC);
-		}
-		Que = attributeQLib->FirstChildElement("ChoiceD");
-		if (Que) {
-			const char * choiceD = Que->GetText();
-			printf("Choice D: %s\n", choiceD);
-		}
-		printf("\n\n\nAnswer: %s\n", answer);
-
-		Que = attributeQLib->FirstChildElement("Explain");
-		if (Que) {
-			const char * explain = Que->GetText();
-			printf("Explain: %s\n", explain);
-		}
 	} while (true);
 
 	cin.get();
