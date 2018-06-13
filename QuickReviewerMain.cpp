16beac08e29a665acfef604590e27220174bc005
zhuyali2018 <yali900d@gmail.com>
2018-06-13 08:40:11 +0100

    Version 1.0, a first working version

--- QuickReviewerMain.cpp
+++ QuickReviewerMain.cpp
@@ -5,39 +5,76 @@
 #include <ctime>
 #include <cstdlib>
 #include <stdio.h>
+#include <vector>
+#include "Question.h"
+
 using namespace tinyxml2;
 using namespace std;
+
+vector<Question> GetAllQuestions(XMLElement * parent) {
+	if (!parent) {
+		throw NoDataException();
+	}
+	vector<Question> Qs;
+	XMLElement* QA = parent->FirstChildElement("QA");;
+	while (true) {
+		int id;
+		const char *qtype;
+		const char *category;
+		int countdown;
+		int resetto;
+		
+		QA->QueryIntAttribute("ID", &id);
+		QA->QueryStringAttribute("type", &qtype);
+		QA->QueryStringAttribute("category", &category);
+		QA->QueryIntAttribute("countdown", &countdown);
+		QA->QueryIntAttribute("resetto", &resetto);
+
+		const string question = QA->FirstChildElement("Question")->GetText();
+		const string answer = QA->FirstChildElement("Answer")->GetText();
+		
+		Question q(id, qtype, category, countdown, resetto,question,answer);
+		Qs.push_back(q);
+		XMLElement * QB = (XMLElement *)QA->NextSibling();
+		if (!QB) break;
+		QA = QB;
+	}
+	return Qs;
+}
 int main(int argc, const char ** argv) 
 {
 	cout << "Quick Reviewer version 1.0" << endl;
-	const char * mydoc = "resources/QASheet.txt";
+	
+	if (argc != 2) {
+		cout << "Usage: " << argv[0] << " <questionDataFile>" << endl;
+		return -1;
+	}
 
 	XMLDocument* doc = new XMLDocument();          //in tinyxml2 namespace
-	doc->LoadFile(mydoc);
+	doc->LoadFile(argv[1]);
 
 	int errorID = doc->ErrorID();
 	if (errorID) {
-		printf("XML file '%s' loaded and parsed failed: ErrorID=%d\n", mydoc, errorID);
+		printf("XML file '%s' loaded and parsed failed: ErrorID=%d\n", argv[1], errorID);
 		cin.get();
 		exit(0);
 	}
-	printf("XML file '%s' loaded and parsed successfully\n\n", mydoc);
+	printf("XML file '%s' loaded and parsed successfully\n\n", argv[1]);
 	
-	do {
-		char * str = nullptr;
-		char * cat = nullptr;
-		
-		char name[10];
-	
-		XMLElement* attributeQLib = doc->FirstChildElement(name);
-		if (!attributeQLib) {
-			break;
-		}
-		attributeQLib->QueryStringAttribute("type", (const char **)&str);
-		attributeQLib->QueryStringAttribute("category", (const char **)&cat);
-		
-	} while (true);
-
+	//-----------------------------------------------------------------------------
+	const char * name = "QuestionsAndAnswers";
+	XMLElement* root = doc->FirstChildElement(name);
+	vector<Question> questions = GetAllQuestions(root);
+	for (Question q : questions) {
+		printf("Question ID:%d\n", q.id);
+		printf("Question type:%s\n", q.qtype.c_str());
+		printf("Question cate:%s\n", q.qcategory.c_str());
+		printf("Question countdown:%d\n", q.countdown);
+		printf("Question resetto:%d\n\n", q.resetto);
+		printf("Question : %s\n", q.question.c_str());
+		printf("Answer : %s\n", q.answer.c_str());
+	}
 	cin.get();
+
 	return 0;
 }
