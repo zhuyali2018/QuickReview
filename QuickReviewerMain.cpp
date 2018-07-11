01f20c81457288b22f3a04c40164e348758d83dd
zhuyali2018 <yali900d@gmail.com>
2018-07-11 03:46:46 +0100

    Version 3.6 added list feature among others

--- QuickReviewerMain.cpp
+++ QuickReviewerMain.cpp
@@ -8,11 +8,37 @@
 #include <stdio.h>
 #include <vector>
 #include <map>
+#include <iomanip>
+#include <algorithm>
+#include <array>
+
 #include "Question.h"
 #include "Qusetting.h"
 
 using namespace tinyxml2;
 using namespace std;
+
+unsigned pagesize = 5;
+static inline void trim(std::string &s);
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+		return !std::isspace(ch);
+	}));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+		return !std::isspace(ch);
+	}).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+	ltrim(s);
+	rtrim(s);
+}
 map<int, QuSetting> GetAllQuestionConfigs(XMLElement * parent) {
 	if (!parent) {
 		throw NoDataException();
@@ -177,125 +203,194 @@ void promptAtQuestion() {
 	printf("           Number: jump to question numbered \n");
 	printf("            Enter: Next \n");
 	printf("        r<number>: skip this question for the next <number> round (default to 3 if number omitted)\n");
+	printf("                R: reload data \n");
+	printf("                l: list questions \n");
+	printf("        p<number>: Specify the nubmer of questions per page \n");
 	printf("           q or x: Exit \n");
 	printf("       Your Input: ");
 }
+void browse(vector<Question> questions);
 int main(int argc, const char ** argv) 
 {
 	bool applyConfigfile = false;
 	string newPath = "NewSavedXMLFile.xml";
-	cout << "\n Quick Reviewer version 3.5" << endl << endl;
-	
-	XMLDocument* doc = new XMLDocument();          //in tinyxml2 namespace
-	XMLDocument* docCfg = nullptr;       //For Doc Question Settings only
+	cout << "\n Quick Reviewer version 3.6" << endl << endl;
+	bool reload = true;
+	XMLDocument* doc = nullptr;
+	while (reload) {
+		reload = false;
+	    doc = new XMLDocument();          //in tinyxml2 namespace
+	    XMLDocument* docCfg = nullptr;       //For Doc Question Settings only
 
-	if (argc == 1) {
-		cout << "\n Usage: " << argv[0] << " <questionDataFile> [oldQuestionDataFile]" << endl;
-		cout << "\n Note: oldQuestionDataFile for question rotation settings only" << endl << endl;
-		//return -1;
-		doc->LoadFile(newPath.c_str());
-		if (doc->ErrorID()) {
-			if (doc->ErrorID() == XML_ERROR_FILE_NOT_FOUND) 
-			    doc->LoadFile("QASheet.txt");
-		    if (!doc->ErrorID()) {
-				printf("XML file QASheet.txt loaded and parsed successfully\n\n");
+		if (argc == 1) {
+			cout << "\n Usage: " << argv[0] << " <questionDataFile> [oldQuestionDataFile]" << endl;
+			cout << "\n Note: oldQuestionDataFile for question rotation settings only" << endl << endl;
+			//return -1;
+			doc->LoadFile(newPath.c_str());
+			if (doc->ErrorID()) {
+				if (doc->ErrorID() == XML_ERROR_FILE_NOT_FOUND)
+					doc->LoadFile("QASheet.txt");
+				if (!doc->ErrorID()) {
+					printf("XML file QASheet.txt loaded and parsed successfully\n\n");
+				}
+				else {
+					printf("XML file '%s' and QASheet.txt loading or parsing failed\n\n", newPath.c_str());
+				}
 			}
 			else {
-				printf("XML file '%s' and QASheet.txt loading or parsing failed\n\n", newPath.c_str());
+				printf("XML file '%s' loaded and parsed successfully\n\n", newPath.c_str());
 			}
 		}
 		else {
-			printf("XML file '%s' loaded and parsed successfully\n\n", newPath.c_str());
-		}
-	}
-	else {
-		doc->LoadFile(argv[1]);
-		if (!doc->ErrorID()) {
-			printf("XML file '%s' loaded and parsed succesfully\n\n", argv[1]);
-			//loading config file
-			if (argc == 3) {   //only when the first file loaded successfully
-				docCfg = new XMLDocument();       //For Doc Question Settings only
-				docCfg->LoadFile(argv[2]);
-				int errorID = docCfg->ErrorID();
-				if (errorID) {
-					printf("XML file %s loading or parsing failed: \n"
-						"                 ErrorID: %d\n"
-						"               Error Msg: %s\n"
-						"              Error Line: %d", argv[3], errorID, doc->ErrorName() + 4, doc->ErrorLineNum());
-				}
-				else {
-					printf("Question config file '%s' loaded and parsed succesfully\n\n", argv[2]);
-					applyConfigfile = true;   //set the configfile flag
+			doc->LoadFile(argv[1]);
+			if (!doc->ErrorID()) {
+				printf("XML file '%s' loaded and parsed succesfully\n\n", argv[1]);
+				//loading config file
+				if (argc == 3) {   //only when the first file loaded successfully
+					docCfg = new XMLDocument();       //For Doc Question Settings only
+					docCfg->LoadFile(argv[2]);
+					int errorID = docCfg->ErrorID();
+					if (errorID) {
+						printf("XML file %s loading or parsing failed: \n"
+							"                 ErrorID: %d\n"
+							"               Error Msg: %s\n"
+							"              Error Line: %d", argv[3], errorID, doc->ErrorName() + 4, doc->ErrorLineNum());
+					}
+					else {
+						printf("Question config file '%s' loaded and parsed succesfully\n\n", argv[2]);
+						applyConfigfile = true;   //set the configfile flag
+					}
 				}
 			}
 		}
-	}
 
-	int errorID = doc->ErrorID();
-	if (errorID) {
-		printf("XML file loaded and parsed failed: \n"
-			   "                 ErrorID: %d\n"
-			   "               Error Msg: %s\n"
-			   "              Error Line: %d", errorID,doc->ErrorName()+4, doc->ErrorLineNum());
+		int errorID = doc->ErrorID();
+		if (errorID) {
+			printf("XML file loaded and parsed failed: \n"
+				"                 ErrorID: %d\n"
+				"               Error Msg: %s\n"
+				"              Error Line: %d", errorID, doc->ErrorName() + 4, doc->ErrorLineNum());
+			//cin.get();
+			exit(0);
+		}
+
+		cin.get();    //press Enter to start
+		//-----------------------------------------------------------------------------
+		const char * name = "QuestionsAndAnswers";
+		XMLElement* root = doc->FirstChildElement(name);
+		XMLElement* root1 = nullptr;  //for configfile
+		vector<Question> questions;
+
+		if (applyConfigfile) {
+			root1 = docCfg->FirstChildElement(name);
+			questions = GetAllQuestions(root, root1);
+		}
+		else {
+			questions = GetAllQuestions(root);
+		}
 		//cin.get();
-		exit(0);
-	}
-	
-	cin.get();    //press Enter to start
-	//-----------------------------------------------------------------------------
-	const char * name = "QuestionsAndAnswers";
-	XMLElement* root = doc->FirstChildElement(name);
-	XMLElement* root1=nullptr;  //for configfile
-	vector<Question> questions;
+		//loop through each questions
+		char k[32] = { 0 };
+		int questionNo = 1;
+		unsigned qno = 0;
+		int total = questions.size();
+		//for (Question q : questions) {
+		while (true) {
+			if ((qno > 0) && (qno < questions.size()))
+				questionNo = qno;
+			Question &q = questions[questionNo];
+			qno = 0;  //reset goto question No
+			system("CLS");   //clear the screen of command line window
+			printf("[Qid:%d]  Question %d/%d : \n%s\n", q.id, questionNo++, total, q.question.c_str());
+			cin.getline(k, 30);       //wait before show answer
 
-	if (applyConfigfile) {
-		root1 = docCfg->FirstChildElement(name);
-		questions = GetAllQuestions(root, root1);
-	}
-	else {
-		questions = GetAllQuestions(root);
-	}
-	//cin.get();
-	//loop through each questions
-	char k[32] = { 0 };
-	int questionNo = 1;
-	int qno = 0;
-	int total = questions.size();
-	//for (Question q : questions) {
-	while(true){
-		if ((qno > 0)&&(qno<questions.size()))
-			questionNo = qno;
-		Question &q = questions[questionNo];
-		qno = 0;  //reset goto question No
-		system("CLS");   //clear the screen of command line window
-		printf("[Qid:%d]  Question %d/%d : \n%s\n", q.id,questionNo++,total, q.question.c_str());	
-		cin.getline(k, 30);       //wait before show answer
+			printf("Answer : \n%s\n", q.answer.c_str());
+			promptAtQuestion();    //show options to exit or job to another questions
 
-		printf("Answer : \n%s\n", q.answer.c_str());
-		promptAtQuestion();    //show options to exit or job to another questions
-		
-		cin.getline(k, 30);
-		qno = atoi(k);		
-		if ((k[0] == 'x') || (k[0] == 'q'))
-			break;
-		else if (k[0] == 'r') {
-			if(k[1]==0)
-			  q.Qc->SetAttribute("resetto", 3);
-			else {
-				char *p = &k[1];
-				int n = atoi(p);
-				if(n>0 && n<999)
-				  q.Qc->SetAttribute("resetto", n);
+			cin.getline(k, 30);
+			qno = atoi(k);
+			if ((k[0] == 'x') || (k[0] == 'q'))
+				break;
+			else if (k[0] == 'r') {
+				if (k[1] == 0)
+					q.Qc->SetAttribute("resetto", 3);
+				else {
+					char *p = &k[1];
+					int n = atoi(p);
+					if (n > 0 && n < 999)
+						q.Qc->SetAttribute("resetto", n);
+				}
+			}
+			else if (k[0] == 'R') {
+				delete doc;
+				reload = true;
+				break;
+			}
+			else if (k[0] == 'l') {   //list all questions				
+				browse(questions);				
+			}
+			else if (k[0] == 'p') {   //list all questions				
+				pagesize = atoi(k + 1);
+				if (pagesize < 5)
+					pagesize = 5;
 			}
 		}
+		if (!reload)
+			break;
 	}
 	
 	doc->SaveFile(newPath.c_str());
-	errorID = doc->ErrorID();
+	int errorID = doc->ErrorID();
 	if (errorID) {
 		printf("\nXML file '%s' Save failed: ErrorID=%d\n", argv[1], errorID);		
 		exit(0);
 	}
 	printf("\nXML file '%s' Saved successfully: ErrorID=%d\n", newPath.c_str(), errorID);
 	return 0;
+}
+void display(vector<Question>::iterator it) {
+	string question = it->question;
+	trim(question);
+	cout << right << setw(4) << it->id << " " << question.c_str() ;
+	cout << endl;
+}
+void browse(vector<Question> questions) {
+	//search using phone number and list the matched records
+	char k[32] = { 0 };
+	auto i= 0;
+	for (vector<Question>::iterator it = questions.begin(); it != questions.end(); ++it) {
+		display(it);
+		if (i >= pagesize) {
+			while (true) {
+				cin.getline(k, 30);
+				if (k[0] == 'q')
+					return;
+				else if (k[0] == 'g') {
+					int id = atoi(k + 1);
+					it = it + id;
+					break;
+				}
+				else if (k[0] == 'p') {   //list all questions				
+					pagesize = atoi(k + 1);
+					if (pagesize < 5)
+						pagesize = 5;
+					break;
+				}
+				else if (k[0] == 'i') {   //go to id			
+					unsigned questionNo = atoi(k + 1);
+					Question &q = questions[questionNo];
+					system("CLS");   //clear the screen of command line window
+					printf("[Qid:%d]  Question %d : \n%s\n", q.id, questionNo++, q.question.c_str());
+					printf("Answer : \n%s\n", q.answer.c_str());
+					it -= pagesize;     //after examin the record, the list postion should keep the same
+				}
+				else {
+					break;
+				}
+			}
+			i = 0;
+		}
+		++i;
+	}
+	cin.get();
 }
