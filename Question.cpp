72011dc90fa300eb4c67a99b5b035cafa4c57904
Yali Zhu <yali@yalimacmini5.attlocal.net>
2019-11-19 22:17:36 -0500

    Added source field to question

--- Question.cpp
+++ Question.cpp
@@ -1,9 +1,8 @@
 #include "Question.h"
 
 
-
-Question::Question(int id, std::string type, std::string category, int cd, int rt, std::string question, std::string answer)
-	:id(id),qtype(type),qcategory(category),countdown(cd),resetto(rt),question(question),answer(answer)
+Question::Question(int id, std::string type, std::string category, int cd, int rt, std::string source, std::string question, std::string answer)
+	:id(id),qtype(type),qcategory(category),countdown(cd),resetto(rt),qsource(source),question(question),answer(answer)
 {
 }
 
--- Question.h
+++ Question.h
@@ -5,12 +5,13 @@ using namespace tinyxml2;
 class Question
 {
 public:
-	Question(int id, std::string type, std::string category, int cd, int rt, std::string question, std::string answer);
+	Question(int id, std::string type, std::string category, int cd, int rt, std::string source, std::string question, std::string answer);
 	~Question();
 //private:
 	int id;
 	std::string qtype;
 	std::string qcategory;
+	std::string qsource;
 	int countdown;
 	int resetto;
 	std::string question;
